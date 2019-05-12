519253fcdd5416dcc5cf88756fc81da8af2494cd
guodongxiaren <879231132@qq.com>
2019-05-12 20:42:18 +0800

    no encrypt m3u8

--- m3u8/m3u8.py
+++ m3u8/m3u8.py
@@ -9,13 +9,14 @@ import wget
 from urllib import parse
 from queue import Queue
 
+
 class M3U8(object):
 
     def __init__(self):
         self.ts_list = []
         self.ts_dict = {}
-        self.q = Queue() 
-
+        self.q = Queue()
+        self.faild_list = []
 
     def get_ts_list(self, url):
         """
@@ -37,17 +38,19 @@ class M3U8(object):
         return ts_list
 
     def build_ts_index(self, ts_list=None):
-        if ts_list == None:
+        if ts_list is None:
             ts_list = self.ts_list
 
-        for i, value in enumerate(ts_list):
-            self.ts_dict[value] = i
+        max_char_len = len(str(len(ts_list)))
 
-        print(self.ts_dict)
+        for i, value in enumerate(ts_list):
+            index = str(i)
+            index = index.zfill(max_char_len)
+            self.ts_dict[value] = index
 
     def download(self, ts_url, dest_dir):
         index = self.ts_dict[ts_url]
-        output = '%s/%s.mp4' % (dest_dir, index)
+        output = '%s/%s.ts' % (dest_dir, index)
         print(ts_url, output)
         wget.download(ts_url, out=output)
 
@@ -60,18 +63,48 @@ class M3U8(object):
             ts_url = self.q.get()
             self.download(ts_url, dest_dir)
 
-    def download_parallel(self, dest_dir):
-        for ts_url in self.ts_list:
-            self.q.put(ts_url)
+    def download_from_list(self, ts_list, dest_dir):
+        for ts_url in ts_list:
+            try:
+                self.download(ts_url, dest_dir)
+            except Exception as e:
+                self.failed_list.append(ts_url)
 
-        t_num = 5
+    def download_parallel(self, dest_dir):
+        list_file = open(f'{dest_dir}/list.txt', 'w')
+        for ts_url in self.ts_list:
+            index = self.ts_dict[ts_url]
+            self.q.put(ts_url)
+            list_file.write(f'file {index}.ts\n')
+        list_file.close()
+
+        num = 10
         p_list = []
-        for j in range(t_num):
-            p = multiprocessing.Process(target=self.download_from_queue, args=(dest_dir))
+        ts_size = len(self.ts_list)
+        size = int(ts_size / num)
+        for j in range(num):
+            begin, end = j * size, (j + 1) * size
+            if j + 1 == num:
+                end = None
+
+            print(begin, end, size)
+            sub_ts_list = self.ts_list[begin:end]
+            p = multiprocessing.Process(
+                target=self.download_from_list, args=(
+                    sub_ts_list, dest_dir))
             p.start()
             p_list.append(p)
-        
-        map(lambda p:p.join(), p_list)
+
+        #map(lambda p:p.join(), p_list)
+        for p in p_list:
+            p.join()
+
+    def retry_failed_ts(self, dest_dir):
+        if len(self.failed_list) == 0:
+            return
+        print('retry failed:')
+        print(m.failed_list)
+        self.download_from_list(self.failed_list, dest_dir)
 
 
 def main(*args):
@@ -82,8 +115,10 @@ def main(*args):
     m.build_ts_index()
     start = time.time()
     m.download_parallel('tmp')
+    m.retry_failed_ts('tmp')
     end = time.time()
     print('end - start', end - start)
+
 
 if __name__ == '__main__':
     main(*sys.argv[1:])
