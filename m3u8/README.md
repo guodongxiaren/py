31656bf1a676add0ac2f13f9183948009f9241a4
guodongxiaren <879231132@qq.com>
2019-11-03 19:29:49 +0800

    extract key

--- m3u8/README.md
+++ m3u8/README.md
@@ -1,6 +1,6 @@
 非加密m3u8视频下载
 =======
-下载完的ts文件直接合并成mp4可能卡顿，可以先合并成ffmpeg，再转码成mp4。
+下载完的ts文件直接合并成mp4可能卡顿，可以先合并成mkv，再转码成mp4。
 另外直接用cat命令合并效果并不理想，还推荐ffmpeg
 ```
 ffmpeg -f concat -safe 0 -i list.txt -c copy output.mkv
@@ -12,3 +12,8 @@ file 000.ts
 file 001.ts
 file 002.ts
 ```
+
+TODO:
+=====
+key解密ts
+重构下载函数, 支持无域名
--- m3u8/m3u8.py
+++ m3u8/m3u8.py
@@ -18,19 +18,40 @@ class M3U8(object):
         self.ts_dict = {}
         self.q = Queue()
         self.failed_list = []
+        self.base_url = ''
+
+    def extract_key(self, line):
+        """
+        提出出key的路径
+        """
+        for data in line.split(','):
+            data = data.replace('"', '')
+            index = data.find("URI=")
+            if index == -1:
+                continue
+            key_path = data[index+5:]
+            return key_path
+        return None
 
     def get_ts_list(self, url):
         """
         url: m3u8资源url
         """
+        filename = url.split('/')[-1]
+        tmp_dir = url.split('/')[-2]
+        filename = 'tmp/'+tmp_dir +'/' + filename
         response = requests.get(url)
         resp = response.text
+        f = open(filename, 'w')
+        f.write(resp)
         ts_list = []
         pr = parse.urlparse(url)
         base = '%s://%s' % (pr.scheme, pr.netloc)
-        print(base)
-        for line in resp.split('\n'):
+        self.base_url = base
+        for line in open(filename, 'r'):
             line = line.strip()
+            if (line.startswith('#EXT-X-KEY')):
+                key_path = self.extract_key(line)
             if len(line) == 0 or line[0] == '#':
                 continue
             ts_url = base + line
@@ -52,8 +73,11 @@ class M3U8(object):
     def download(self, ts_url, dest_dir):
         index = self.ts_dict[ts_url]
         output = '%s/%s.ts' % (dest_dir, index)
-        print(ts_url, output)
-        wget.download(ts_url, out=output)
+        print(ts_url, output, '\n')
+        #wget.download(ts_url, out=output)
+        with open(output, 'wb') as f:
+            response = requests.get(ts_url, timeout=30)
+            f.write(response.content)
 
     def download_all(self, dest_dir):
         for ts_url in self.ts_list:
@@ -113,7 +137,8 @@ def main(*args):
     tmp_dir = url.split('/')[-2]
     print(url, tmp_dir)
     tmp_path = f'tmp/{tmp_dir}'
-    os.makedirs(tmp_path)
+    if not os.path.exists(tmp_path):
+        os.makedirs(tmp_path)
     m = M3U8()
     m.get_ts_list(url)
     m.build_ts_index()
