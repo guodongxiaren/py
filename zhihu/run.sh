128ebd16935f6a07291f3f35b04e83c768dda84f
guodongxiaren <879231132@qq.com>
2020-05-30 13:52:24 +0800

    fix ip

--- zhihu/run.sh
+++ zhihu/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+IP=124.64.11.34
+curl --header "X-Forwarded-For: $IP" https://www.zhihu.com/api/v4/members/guodongxiaren?include=follower_count%2cvoteup_count > mine.json
+exit
 python zhihu.py >> data.txt
 echo "date,voteup,follower" > data.csv
 tac data.txt >> data.csv
--- zhihu/zhihu.py
+++ zhihu/zhihu.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python
 # coding=utf-8
 import datetime
-import urllib
-from bs4 import BeautifulSoup
-
-url = 'https://www.zhihu.com/people/guodongxiaren/'
-origin_bytes = urllib.urlopen(url).read()
-origin_string = origin_bytes.decode('utf-8')
-print(origin_string)
-soup = BeautifulSoup(origin_string, 'html.parser')
-
-meta_list = soup.find_all('meta')
-voteupCount, followerCount = 0,0
-for meta in meta_list:
-    if meta.get('itemprop') == 'zhihu:voteupCount':
-        voteupCount = meta.get('content')
-    elif meta.get('itemprop') == 'zhihu:followerCount':
-        if meta.has_attr('content'):
-            followerCount = meta.get('content')
+import requests
+header = {
+        'Host': 'www.zhihu.com',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept': '*/*',
+        'User-Agent': 'PostmanRuntime/7.17.1',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+}
+url = 'https://www.zhihu.com/api/v4/members/guodongxiaren?include=follower_count%2cvoteup_count'
+resp = requests.get(url, headers=header)
+data = resp.json()
+voteupCount = data['voteup_count']
+followerCount = data['follower_count']
 
 date = datetime.datetime.now().strftime('%Y-%m-%d')
-print '%s,%s,%s' % (date, voteupCount, followerCount)
+line = '%s,%s,%s' % (date, voteupCount, followerCount)
+print(line)
